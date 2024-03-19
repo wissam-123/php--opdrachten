ade6bffad0deb2c86320965d0d4e631e1530e2e3
wissam-123 <144032700+wissam-123@users.noreply.github.com>
2024-03-19 21:23:40 +0100

    Add files via upload

---
+++ crud_bier/config.php
@@ -0,0 +1,12 @@
+<?php
+// auteur: Wissam
+// functie: configuratiebestand
+
+define("DATABASE", "bieren");
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+define("CRUD_TABLE", "bier");
+
+?>
